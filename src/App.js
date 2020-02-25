477ce6132b98473246c1f33a729c94918d04bc66
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-02-25 12:19:33 +0200

    Add initial setup with working api fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const InputForm = ({handleClick, article, handleInput}) => (
+  <div>
+    <form onSubmit={handleClick}>
+      <div>
+        Artikkeli: <input value={article}
+                     onChange={handleInput} />
+      </div>
+      <div>
+        <button type="submit">Etsi reitti</button>
+      </div>
+    </form>
+  </div>
+)
+
+const parseNeighborData = (data) => {
+  var pageid = Object.keys(data['query']['pages'])[0]
+  var links = data['query']['pages'][pageid]['links']
+  return links.map(link => link.title)
 }
 
-export default App;
+const getNeighbors = async (node) => {
+  const URL_BASE = "https://fi.wikipedia.org/w/api.php?"
+  const URL_PARAMS = "action=query&prop=links&pllimit=100&format=json&origin=*"
+  const response = await axios.get(`${URL_BASE}${URL_PARAMS}&titles=${node}`)
+  const neighbors = parseNeighborData(response.data)
+  console.log(neighbors)
+}
+
+const App = () => {
+  const [ article, setArticle ] = useState('')
+  const [ result, setResult ] = useState([])
+  var neighbors = []
+
+  const search = () => {
+    getNeighbors("Hanau")
+  }
+
+  const handleInput = (event) => {
+    setArticle(event.target.value)
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    search();
+    var res = ['Hanau','Suomi']
+    setResult(result.concat(res))
+  }
+    
+  return (
+    <div>
+      <h1>Wikipeli</h1>
+      <p>Anna Wikipedia-artikkelin nimi (esimerkiksi Helsinki) ja Wikipeli löytää nopeimman reitin antamastasi artikkelista <a href="https://fi.wikipedia.org/wiki/Jeesus">Jeesus-artikkeliin</a>.</p>
+    <InputForm 
+      handleClick={ handleClick }
+      article={ article }
+      handleInput={ handleInput } />
+    </div>
+  )
+}
+
+export default App
